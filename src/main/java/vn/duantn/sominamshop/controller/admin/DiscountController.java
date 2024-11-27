d809b8b7500028021f9027c4ff08cf8c798e3310
2704trang <quynhtrangqt2704@gmail.com>
2024-11-27 23:36:01 +0700

    update

---
+++ src/main/java/vn/duantn/sominamshop/controller/admin/DiscountController.java
@@ -0,0 +1,11 @@
+package vn.duantn.sominamshop.controller.admin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class DiscountController {
+
+    @GetMapping("/khuyenmai")
+    public void khuyenMai(){
+
+    }
+}
