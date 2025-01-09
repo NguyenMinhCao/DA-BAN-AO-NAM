12e06d47f70fbabef73577162d4ac13a512362b0
NguyenMinhCao <caonguyenlaptrinh@gmail.com>
2025-01-09 08:47:02 +0700

    update

--- src/main/java/vn/duantn/sominamshop/controller/client/ItemController.java
+++ src/main/java/vn/duantn/sominamshop/controller/client/ItemController.java
@@ -1,19 +1,25 @@
 package vn.duantn.sominamshop.controller.client;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.duantn.sominamshop.model.Color;
 import vn.duantn.sominamshop.model.Product;
 import vn.duantn.sominamshop.model.ProductDetail;
+import vn.duantn.sominamshop.model.Size;
 import vn.duantn.sominamshop.service.AddressService;
 import vn.duantn.sominamshop.service.CartService;
 import vn.duantn.sominamshop.service.ProductDetailService;
 import vn.duantn.sominamshop.service.ProductService;
 import vn.duantn.sominamshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ItemController {
@@ -42,9 +48,41 @@ public class ItemController {
     public String getProductDetail(Model model, @PathVariable long id) {
         Product product = this.productService.findProductByIdWithImg(id);
         List<ProductDetail> lstProductDetail = this.productDetailService.findProductDetailByProducts(product);
+
+        List<Color> lstColorInProDetail = new ArrayList<>();
+        List<Size> lstSizeInProDetail = new ArrayList<>();
+
+        Set<Long> sizeIds = new LinkedHashSet<>();
+        Set<Long> colorIds = new LinkedHashSet<>(); // Sử dụng LinkedHashSet để giữ thứ tự
+
+        for (ProductDetail productDetail : lstProductDetail) {
+            Size size = productDetail.getSize();
+            Color color = productDetail.getColor();
+            if (!colorIds.contains(color.getId())) {
+                colorIds.add(color.getId());
+                lstColorInProDetail.add(color);
+            }
+
+            if (!sizeIds.contains(size.getId())) {
+                sizeIds.add(size.getId());
+                lstSizeInProDetail.add(size);
+            }
+        }
+
         model.addAttribute("product", product);
         model.addAttribute("lstProductDetail", lstProductDetail);
+        model.addAttribute("lstColorInProDetail", lstColorInProDetail);
+        model.addAttribute("lstSizeInProDetail", lstSizeInProDetail);
         return "client/product/detail";
     }
 
+    @GetMapping("")
+    public String findProductDetailByColor() {
+        return new String();
+    }
+
+    // @GetMapping("/product/{id}")
+    // public String getProductDetail(@RequestParam String param) {
+    // return new String();
+    // }
 }
