bc846d49480624eaf90efc15128d7c69e008fb30
Hoang90cm <hoangchph29401@fpt.edu.vn>
2025-01-14 20:15:46 +0700

    update

--- src/main/java/vn/duantn/sominamshop/repository/ProductRepository.java
+++ src/main/java/vn/duantn/sominamshop/repository/ProductRepository.java
@@ -82,53 +82,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
                         "WHERE dbo.products.id = :productId and dbo.sizes.id = :sizeId", nativeQuery = true)
         Integer quantityBySizeId(@Param("productId") Integer productId, @Param("sizeId") Integer sizeId);
 
-<<<<<<< HEAD
-    List<Product> findByName(String name);
-    @Query(value = "SELECT\n" +
-            "    p.id AS id,\n" +
-            "    p.name AS name,\n" +
-            "    COALESCE(i.url_image, '') AS image,\n" +
-            "    p.description AS description,\n" +
-            "    SUM(pd.quantity) AS quantity,\n" +
-            "    p.status AS status\n" +
-            "FROM\n" +
-            "    products p\n" +
-            "LEFT JOIN\n" +
-            "    product_details pd ON p.id = pd.product_id\n" +
-            "LEFT JOIN\n" +
-            "    images i ON pd.id = i.product_detail_id AND i.is_main = 1\n" +
-            "GROUP BY\n" +
-                    "    p.id, p.name, p.description, p.status, i.url_image\n" +
-                    "ORDER BY p.id", nativeQuery = true)
-    List<ProductResponse> getAll();
-=======
-        @Query("Select Count(p.id) From Product p")
-        int countOrder();
->>>>>>> f67bb72cee4407f40e7ca3aff207ad45bf242854
-
-        @Procedure(name = "hotSelling")
-        List<Object> hotSelling(@Param("minQuantity") int minQuantity);
-
-        @Query("Select p From Product p")
-        List<Product> getListProduct();
-
-        @Query("select c from Product c where c.name like %:name% and c.status = 0")
-        List<Product> searchProductByName(@Param("name") String name);
-
-<<<<<<< HEAD
-
-    @Query(value = "SELECT SUM(dbo.product_details.quantity)\n" +
-            "FROM     dbo.colors INNER JOIN\n" +
-            "                  dbo.product_details ON dbo.colors.id = dbo.product_details.color_id INNER JOIN\n" +
-            "                  dbo.products ON dbo.product_details.product_id = dbo.products.id \n" +
-            "WHERE dbo.products.id = :productId and dbo.colors.id = :colorId",nativeQuery = true)
-    Integer quantityByColorId(@Param("productId") Integer productId,@Param("colorId") Integer colorId);
-    @Query(value = "SELECT SUM(dbo.product_details.quantity)\n" +
-            "FROM     dbo.sizes INNER JOIN\n" +
-            "                  dbo.product_details ON dbo.sizes.id = dbo.product_details.size_id INNER JOIN\n" +
-            "                  dbo.products ON dbo.product_details.product_id = dbo.products.id \n" +
-            "WHERE dbo.products.id = :productId and dbo.sizes.id = :sizeId",nativeQuery = true)
-    Integer quantityBySizeId(@Param("productId") Integer productId,@Param("sizeId") Integer sizeId);
     @Query("Select Count(p.id) From Product p")
     int countOrder();
 
@@ -143,63 +96,5 @@ public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpec
 
 
 
-//    @Query(value = """
-//        SELECT
-//            new vn.duantn.sominamsh.o.model.response.ProductResponseClient(
-//                p.id,
-//                p.name,
-//                COALESCE((SELECT MAX(i.urlImage) FROM Image i WHERE i.product.id = p.id), ''),
-//                p.description,
-//                MIN(pd.price) as minPrice,
-//                MAX(pd.price) as maxPrice,
-//                p.status,
-//                0 AS discount
-//            )
-//        FROM
-//            Product p
-//        LEFT JOIN
-//            p.productDetails pd
-//        LEFT JOIN
-//            p.images i
-//        WHERE
-//            i.id = (SELECT MIN(id) FROM Image WHERE product.id = p.id) AND
-//            p.status = 0
-//        GROUP BY
-//            p.id, p.name, p.description, p.status
-//        ORDER BY
-//            p.id
-//        """)
-//    Page<ProductResponseClient> pageProductResponse(Pageable pageable);
-
-=======
-        // @Query(value = """
-        // SELECT
-        // new vn.duantn.sominamsh.o.model.response.ProductResponseClient(
-        // p.id,
-        // p.name,
-        // COALESCE((SELECT MAX(i.urlImage) FROM Image i WHERE i.product.id = p.id),
-        // ''),
-        // p.description,
-        // MIN(pd.price) as minPrice,
-        // MAX(pd.price) as maxPrice,
-        // p.status,
-        // 0 AS discount
-        // )
-        // FROM
-        // Product p
-        // LEFT JOIN
-        // p.productDetails pd
-        // LEFT JOIN
-        // p.images i
-        // WHERE
-        // i.id = (SELECT MIN(id) FROM Image WHERE product.id = p.id) AND
-        // p.status = 0
-        // GROUP BY
-        // p.id, p.name, p.description, p.status
-        // ORDER BY
-        // p.id
-        // """)
-        // Page<ProductResponseClient> pageProductResponse(Pageable pageable);
->>>>>>> f67bb72cee4407f40e7ca3aff207ad45bf242854
 
 }
